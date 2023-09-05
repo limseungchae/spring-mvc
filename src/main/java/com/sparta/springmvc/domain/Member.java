16e4124a8ba1d922066d39412541bdc3bef4a22e
admin <tmdco12@naver.com>
2023-09-05 19:47:25 +0900

    "feat: Member 엔티티 클래스 추가
    Member 엔티티 클래스가 추가되었습니다. 이 클래스는 회원 정보를 저장하기 위한 모델로 사용되며, id와 name 필드를 포함하고 있습니다."

---
+++ src/main/java/com/sparta/springmvc/domain/Member.java
@@ -0,0 +1,27 @@
+package com.sparta.springmvc.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    // Getter for id
+    public Long getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
